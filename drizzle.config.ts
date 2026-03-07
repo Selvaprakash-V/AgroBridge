9121718bf07ccc51212857c42cb85d4a08dceeb9
Selvaprakash-V <selvaprakashv007@gmail.com>
2026-03-07 23:12:40 +0530

    Update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
 dotenv.config({ path: ".env.local" });
-
 export default {
   schema: "./drizzle/schema.ts",
   out: "./drizzle/migrations",
